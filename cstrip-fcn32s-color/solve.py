34e873442392fa90fbd62d3bad6313674b82cd56
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-08-19 13:51:04 +1000

    code cleanup, and made paths variable depending on wheere they are run (hpc or locally)

--- cstrip-fcn32s-color/solve.py
+++ cstrip-fcn32s-color/solve.py
@@ -2,37 +2,37 @@
 # import caffe
 import numpy as np
 import os, sys
+from os.path import expanduser
 import imp
-# import setproctitle
-# setproctitle.setproctitle(os.path.basename(os.getcwd()))
 
 # add '../' directory to path for importing score.py, surgery.py and pycaffe layer
 file_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 sys.path.append(file_location[:file_location.rfind('/')])
+home_dir = expanduser("~")
 
 # import support functions
-caffe_root = '/home/n8307628/Fully-Conv-Network/Resources/caffe'
+if 'n8307628' in home_dir:
+    caffe_root = home_dir+'/Fully-Conv-Network/Resources/caffe'
+elif 'sean' in home_dir:
+    caffe_root = home_dir+'/src/caffe'
 filename, path, desc =  imp.find_module('caffe', [caffe_root+'/python/'])
 caffe = imp.load_module('caffe', filename, path, desc)
 caffe.set_mode_cpu()
 # caffe.set_device(1)
-# with caffe found load remaining files (they use caffe too)
-
-
-weights = file_location+'/nyud-fcn32s-color-heavy.caffemodel'
+import surgery, score
 
 # init
+weights = file_location+'/nyud-fcn32s-color-heavy.caffemodel'
 solver = caffe.SGDSolver(file_location+'/solver.prototxt')
 solver.net.copy_from(weights)
 
 # surgeries
-import surgery, score
 interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
 print 'performing surgery'
 surgery.interp(solver.net, interp_layers)
 #
 # # scoring
-val = np.loadtxt('/work/cyphy/Datasets/NYU2/val.txt', dtype=str)
+val = np.loadtxt(home_dir+'/Construction_Site/Springfield/12Aug16/K2/val.txt', dtype=str)
 
 for _ in range(50):
     print 'Running solver.step iter {}'.format(_)
