a09da5ff10759cb9ddb790c90ddd8bf73c178f16
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-08-19 15:57:03 +1000

    modified to work with .mat files. Also cleaned up code and added inputs for easier implimentation

--- data/cs-trip/createDataSplits.py
+++ data/cs-trip/createDataSplits.py
@@ -24,8 +24,8 @@ import os.path
 # e.g. 0000-5_1470962221647243797
 
 def getImgIndex(filename):
-    # sample string: colourimg_0459-5_1470961026872523873.png
-    return filename[filename.find('_')+1:filename.rfind('_')]
+    # sample string: colourimg_0459-5_1470961026872523873_objects.mat
+    return filename[filename.find('_')+1:filename.rfind('_',0,filename.rfind('_'))]
 
 def getSubSetName(filename):
     last_slash = filename.rfind('/')
@@ -37,37 +37,41 @@ def getSubSetName(filename):
     return filename[last_slash+1:]
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--dataset_directory', default='/home/sean/hpc-home/Construction_Site/Springfield/12Aug16/K2')
 parser.add_argument('--training_dir', nargs='+', help='List of directories')
 parser.add_argument('--testing_dir', nargs='+')
 parser.add_argument('--val_percentage', default=0.15)
-parser.add_argument('--out_dir', default='/home/sean/hpc-home/Construction_Site/Springfield/12Aug16')
+parser.add_argument('--out_dir', default='/home/sean/Dropbox/Uni/Code/FCN_models/data/cs-trip')
 args = parser.parse_args()
+training_dir = args.training_dir
+testing_dir = args.testing_dir
+
+if testing_dir in training_dir:
+    print 'WARNING: same data subsets used in training and testing'
 
 # Get train directory and indices
 print 'Getting train directory and indecies'
 train_dir_index = []; test_dir_index = []
-for train_path in args.training_dir:
+for train_path in training_dir:
     current_training_dir = getSubSetName(train_path)
-    img_fullnames = glob.glob(train_path+'/colour/*.png')
+    img_fullnames = glob.glob(args.dataset_directory + '/'+ train_path+'/labels/*.mat')
     for image_name in img_fullnames:
         img_index = getImgIndex(os.path.basename(image_name))
         train_dir_index.append([current_training_dir, img_index])
 
 # Get test directory and indecies
 print 'Getting test directory and indecies'
-for test_path in args.testing_dir:
+for test_path in testing_dir:
     current_testing_dir = getSubSetName(test_path)
-    img_fullnames = glob.glob(test_path+'/colour/*.png')
+    img_fullnames = glob.glob(args.dataset_directory + '/'+ test_path+'/labels/*.mat')
     for image_name in img_fullnames:
         img_index = getImgIndex(os.path.basename(image_name))
         test_dir_index.append([current_testing_dir, img_index])
 
 # create val directory and indecies list
-print 'Extracting val directory and indices from training set'
 num_val_instances = int( (args.val_percentage * len(train_dir_index)) /2)
-print num_val_instances
-val_dir_index = train_dir_index[0:num_val_instances]
-val_dir_index.append(train_dir_index[-num_val_instances:])
+print 'Extracting val directory and indices from training set ({}% ({}) of training images)'.format(args.val_percentage*100, num_val_instances)
+val_dir_index = train_dir_index[0:num_val_instances] + train_dir_index[-num_val_instances:]
 train_dir_index[0:num_val_instances] = []
 train_dir_index[-num_val_instances:] = []
 
