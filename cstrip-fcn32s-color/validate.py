da6ea789718d3a6c993e1c170c4ab00658eb5121
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-08-23 08:39:11 +1000

    validate (maybe test) the trained networks. Doing training and validation in the same file is too much computation for even HPC nodes

---
+++ cstrip-fcn32s-color/validate.py
@@ -0,0 +1,55 @@
+#! /usr/bin/python
+"""
+cstrip validate COLOUR only
+
+"""
+# import caffe
+import numpy as np
+import os, sys
+from os.path import expanduser
+import imp
+import argparse
+
+# add '../' directory to path for importing score.py, surgery.py and pycaffe layer
+file_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+sys.path.append(file_location[:file_location.rfind('/')])
+home_dir = expanduser("~")
+
+# User Input
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', default='gpu')
+parser.add_argument('--iteration',default=8000)
+args = parser.parse_args()
+iteration = args.iteration
+print 'This is the COLOUR only validation!'
+
+# import support functions
+if 'n8307628' in home_dir:
+    caffe_root = home_dir+'/Fully-Conv-Network/Resources/caffe'
+    weights = file_location+'/colorSnapshot/_iter_'+ str(iteration) +'.caffemodel'
+elif 'sean' in home_dir:
+    caffe_root = home_dir+'/src/caffe'
+    weights = home_dir+'/hpc-home/Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color/colorSnapshot/_iter_'+ str(iteration) +'.caffemodel'
+filename, path, desc =  imp.find_module('caffe', [caffe_root+'/python/'])
+caffe = imp.load_module('caffe', filename, path, desc)
+if 'g' in args.mode or 'G' in args.mode:
+    caffe.set_mode_gpu()
+    print '-- GPU Mode -- {}'.format(args.mode)
+elif 'c' in args.mode or 'C' in args.mode:
+    caffe.set_mode_cpu()
+    print '-- CPU Mode -- {}'.format(args.mode)
+else:
+    caffe.set_mode_gpu()
+    print '==============='
+    print 'No Mode (CPU or GPU) Given'
+    print '-- GPU Mode Chosen --'
+    print '==============='
+# caffe.set_device(1)
+import surgery, score
+
+# init
+solver = caffe.SGDSolver(file_location+'/solver.prototxt')
+solver.net.copy_from(weights)
+
+print '\n>>>> Validation <<<<\n'
+score.seg_tests(solver, False, val, layer='score')
