155f7658d93e2832df8f9805fc4cb63aa7d3059f
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-08-22 11:53:49 +1000

    added code to help differentiate this from the color-depth network training

--- cstrip-fcn32s-color/solve.py
+++ cstrip-fcn32s-color/solve.py
@@ -1,14 +1,25 @@
 #! /usr/bin/python
+"""
+cstrip COLOUR only
+
+"""
 # import caffe
 import numpy as np
 import os, sys
 from os.path import expanduser
 import imp
+import argparse
 
 # add '../' directory to path for importing score.py, surgery.py and pycaffe layer
 file_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 sys.path.append(file_location[:file_location.rfind('/')])
 home_dir = expanduser("~")
+
+# User Input
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', default='gpu')
+args = parser.parse_args()
+print 'This is the COLOUR only solver!'
 
 # import support functions
 if 'n8307628' in home_dir:
@@ -19,7 +30,18 @@ elif 'sean' in home_dir:
     weights = home_dir+'/hpc-home/Fully-Conv-Network/Resources/FCN_models/pretrained_weights/nyud-fcn32s-color-heavy.caffemodel'
 filename, path, desc =  imp.find_module('caffe', [caffe_root+'/python/'])
 caffe = imp.load_module('caffe', filename, path, desc)
-caffe.set_mode_gpu()
+if 'g' or 'G' in args.mode:
+    caffe.set_mode_gpu()
+    print '-- GPU Mode --'
+elif 'c' or 'C' in args.mode:
+    caffe.set_mode_cpu()
+    print '-- CPU Mode --'
+else:
+    caffe.set_mode_gpu()
+    print '==============='
+    print 'No Mode (CPU or GPU) Given'
+    print '-- GPU Mode Chosen --'
+    print '==============='
 # caffe.set_device(1)
 import surgery, score
 
@@ -43,5 +65,5 @@ for _ in range(50):
     # if getting issues on HPC try
     # export MKL_CBWR=AUTO
     # and 'export CUDA_VISIBLE_DEVICES=1'
-    print '\n>>>> Validation <<<<\n'
-    score.seg_tests(solver, False, val, layer='score')
+    # print '\n>>>> Validation <<<<\n'
+    # score.seg_tests(solver, False, val, layer='score')
