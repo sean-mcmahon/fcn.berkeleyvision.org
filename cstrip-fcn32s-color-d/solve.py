43c3297fa05f68e7aaf56f1e33035c1db8131030
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-08-25 10:00:27 +1000

    giving the network a the color tripnet as initialisations

--- cstrip-fcn32s-color-d/solve.py
+++ cstrip-fcn32s-color-d/solve.py
@@ -24,10 +24,10 @@ print 'This is the colour-DEPTH solver!'
 # import support functions
 if 'n8307628' in home_dir:
     caffe_root = home_dir+'/Fully-Conv-Network/Resources/caffe'
-    weights = home_dir+'/Fully-Conv-Network/Resources/FCN_models/pretrained_weights/nyud-fcn32s-color-heavy.caffemodel'
+    weights = home_dir+'/Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color/colorSnapshot/_iter_2000.caffemodel'
 elif 'sean' in home_dir:
     caffe_root = home_dir+'/src/caffe'
-    weights = home_dir+'/hpc-home/Fully-Conv-Network/Resources/FCN_models/pretrained_weights/nyud-fcn32s-color-heavy.caffemodel'
+    weights = home_dir+'/hpc-home/Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color/colorSnapshot/_iter_2000.caffemodel'
 filename, path, desc =  imp.find_module('caffe', [caffe_root+'/python/'])
 caffe = imp.load_module('caffe', filename, path, desc)
 if 'g' in args.mode or 'G' in args.mode:
