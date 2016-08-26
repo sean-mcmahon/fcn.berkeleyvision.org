45508669167c93c1790eb7e4b0cb0b61f901eb5f
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-08-26 17:25:00 +1000

    modifiing paths for my cs dataset

--- cstrip-fcn32s-color-hha/solve.py
+++ cstrip-fcn32s-color-hha/solve.py
@@ -1,22 +1,58 @@
-import caffe
-import surgery, score
+#! /usr/bin/python
+"""
+cstrip Color and HHA
 
+"""
+# import caffe
 import numpy as np
-import os
+import os, sys
+from os.path import expanduser
+import imp
+import argparse
 
-import setproctitle
-setproctitle.setproctitle(os.path.basename(os.getcwd()))
+# add '../' directory to path for importing score.py, surgery.py and pycaffe layer
+file_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+sys.path.append(file_location[:file_location.rfind('/')])
+home_dir = expanduser("~")
 
-color_proto = '../nyud-rgb-32s/trainval.prototxt'
-color_weights = '../nyud-rgb-32s/nyud-rgb-32s-28k.caffemodel'
-hha_proto = '../nyud-hha-32s/trainval.prototxt'
-hha_weights = '../nyud-hha-32s/nyud-hha-32s-60k.caffemodel'
+# User Input
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', default='CPU')
+args = parser.parse_args()
+print 'This is the colour-HHA solver!'
 
-# init
-caffe.set_device(int(sys.argv[1]))
-caffe.set_mode_gpu()
+# import support functions
+if 'n8307628' in home_dir:
+    caffe_root = home_dir+'/Fully-Conv-Network/Resources/caffe'
 
-solver = caffe.SGDSolver('solver.prototxt')
+    color_weights = home_dir+'/Fully-Conv-Network/Resources/FCN_models/pretrained_weights/nyud-fcn32s-color-heavy.caffemodel'
+    color_proto = home_dir+'/Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color/trainval.prototxt'
+    hha_weights = home_dir+'/Fully-Conv-Network/Resources/FCN_models/pretrained_weights/nyud-fcn32s-hha-heavy.caffemodel'
+    hha_proto = home_dir+'/Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color-d/trainval.prototxt'
+elif 'sean' in home_dir:
+    caffe_root = home_dir+'/src/caffe'
+
+    color_weights = home_dir+'/hpc-home/Fully-Conv-Network/Resources/FCN_models/pretrained_weights/nyud-fcn32s-color-heavy.caffemodel'
+    color_proto = home_dir+'/hpc-home/Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color/trainval.prototxt'
+    hha_weights = home_dir+'/hpc-home/Fully-Conv-Network/Resources/FCN_models/pretrained_weights/nyud-fcn32s-hha-heavy.caffemodel'
+    hha_proto = home_dir+'/hpc-home/Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color-d/trainval.prototxt'
+
+filename, path, desc =  imp.find_module('caffe', [caffe_root+'/python/'])
+caffe = imp.load_module('caffe', filename, path, desc)
+if 'g' in args.mode or 'G' in args.mode:
+    caffe.set_mode_gpu()
+    print '-- GPU Mode -- {}'.format(args.mode)
+elif 'c' in args.mode or 'C' in args.mode:
+    caffe.set_mode_cpu()
+    print '-- CPU Mode -- {}'.format(args.mode)
+else:
+    caffe.set_mode_gpu()
+    print '==============='
+    print 'No Mode (CPU or GPU) Given'
+    print '-- GPU Mode Chosen -- {}'.format(args.mode)
+    print '==============='
+import surgery, score
+solver = caffe.SGDSolver(file_location+'/solver.prototxt')
 
 # surgeries
 color_net = caffe.Net(color_proto, color_weights, caffe.TEST)
@@ -28,6 +64,7 @@ surgery.transplant(solver.net, hha_net, suffix='hha')
 del hha_net
 
 interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+print 'performing surgery on {}'.format(interp_layers)
 surgery.interp(solver.net, interp_layers)
 
 # scoring
@@ -35,4 +72,4 @@ test = np.loadtxt('../data/nyud/test.txt', dtype=str)
 
 for _ in range(50):
     solver.step(2000)
-    score.seg_tests(solver, False, val, layer='score')
+    # score.seg_tests(solver, False, val, layer='score')
