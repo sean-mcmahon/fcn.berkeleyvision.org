c7e79a8ceac9dd26d255b4e981a24a78688d7337
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-08-30 10:49:49 +1000

    further setup of deploy, ready for testing

--- deploy.py
+++ deploy.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 """
-cstrip validate general
+cstrip deploy_prototxt
 
 """
 # import caffe
@@ -12,6 +12,7 @@ import imp
 import argparse
 import glob
 import score
+from PIL import Image
 
 
 def add_slash(mystring):
@@ -40,17 +41,20 @@ network_dir = args.network_dir
 network_dir = add_slash(network_dir)  # ensure slash present
 snapshot_filter = args.snapshot_filter
 test_type = args.test_type
-print 'This is the general validation script!'
+print 'This is the general deploy_prototxt script!'
 
 # import support functions
 if 'n8307628' in home_dir:
     caffe_root = home_dir + '/Fully-Conv-Network/Resources/caffe'
     working_dir = home_dir + '/Fully-Conv-Network/Resources/FCN_models/' \
         + network_dir
+    cstrip_dir = home_dir + '/Construction_Site/Springfield/12Aug16/K2'
 elif 'sean' in home_dir:
     caffe_root = home_dir + '/src/caffe'
     working_dir = home_dir + \
         '/hpc-home/Fully-Conv-Network/Resources/FCN_models/' + network_dir
+    cstrip_dir = home_dir + \
+        '/hpc-home/Construction_Site/Springfield/12Aug16/K2'
 filename, path, desc = imp.find_module('caffe', [caffe_root + '/python/'])
 caffe = imp.load_module('caffe', filename, path, desc)
 if 'g' in args.mode or 'G' in args.mode:
@@ -65,10 +69,26 @@ else:
     print 'No Mode (CPU or GPU) Given'
     print '-- GPU Mode Chosen --'
     print '==============='
-# caffe.set_device(1)
+
 snapshot_dir = glob.glob(working_dir + '*napshot*')
 deploy_prototxt = glob.glob(working_dir + '*' + test_type + '*.prototxt')
 weights = snapshot_dir[0] + '/' + snapshot_filter + \
     '_iter_' + str(iteration) + '.caffemodel'
+test_set = np.loadtxt(file_location +
+                      '/data/cs-trip/train.txt', dtype=str)
+save_dir = working_dir + test_type + '_deploy'
 
-fcn = caffe.Net()
+fcn = caffe.Net(deploy_prototxt[0], weights, caffe.TEST)
+if not os.path.isdir(save_dir):
+    os.mkdir(save_dir)
+layer = 'score'
+for idx in test_set:
+    fcn.forward()
+    im = Image.fromarray(fcn.blobs[layer].data[0].argmax(
+        0).astype(np.uint8) * 255, mode='P')
+    colorIm = Image.open(
+        glob.glob('{}/{}/colour/colourimg_{}_*'.format(cstrip_dir, idx[0],
+                                                       idx[1]))[0])
+    overlay = Image.blend(colorIm.convert(
+        "RGBA"), im.convert("RGBA"), 0.7)
+    overlay.save(os.path.join(save_dir, ''.join(idx) + '.png'))
