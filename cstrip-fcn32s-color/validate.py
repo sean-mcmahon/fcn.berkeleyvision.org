eb7da6eff7f59d3db8c8618e3957bfb65a1b6bef
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-08-30 10:20:56 +1000

    beatified

--- cstrip-fcn32s-color/validate.py
+++ cstrip-fcn32s-color/validate.py
@@ -5,47 +5,51 @@ cstrip validate COLOUR only
 """
 # import caffe
 import numpy as np
-import os, sys
+import os
+import sys
 from os.path import expanduser
 import imp
 import argparse
 import glob
 
+
 def add_slash(mystring):
     if mystring.endswith('/'):
         return mystring
     else:
-        return mystring+'/'
+        return mystring + '/'
 
-# add '../' directory to path for importing score.py, surgery.py and pycaffe layer
-file_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+# add '../' directory to path for importing score.py, surgery.py and
+# pycaffe layer
+file_location = os.path.realpath(os.path.join(
+    os.getcwd(), os.path.dirname(__file__)))
 sys.path.append(file_location[:file_location.rfind('/')])
 home_dir = expanduser("~")
 
 # User Input
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', default='cpu')
-parser.add_argument('--iteration',default=8000)
+parser.add_argument('--iteration', default=8000)
 parser.add_argument('--test_type', default='val')
 parser.add_argument('--network_dir', default='cstrip-fcn32s-color')
 args = parser.parse_args()
 iteration = args.iteration
 network_dir = args.network_dir
-network_dir = add_slash(network_dir) # ensure slash present
+network_dir = add_slash(network_dir)  # ensure slash present
 print 'This is the COLOUR only validation!'
 
 # import support functions
 if 'n8307628' in home_dir:
-    caffe_root = home_dir+'/Fully-Conv-Network/Resources/caffe'
-    weight_dir = home_dir+ '/Fully-Conv-Network/Resources/FCN_models/' + network_dir
-    snapshot_dir = glob.glob(weight_dir+'*napshot*')
-    weights = snapshot_dir[0]+'/_iter_'+ str(iteration) +'.caffemodel'
+    caffe_root = home_dir + '/Fully-Conv-Network/Resources/caffe'
+    weight_dir = home_dir + '/Fully-Conv-Network/Resources/FCN_models/' + network_dir
+    snapshot_dir = glob.glob(weight_dir + '*napshot*')
+    weights = snapshot_dir[0] + '/_iter_' + str(iteration) + '.caffemodel'
 elif 'sean' in home_dir:
-    caffe_root = home_dir+'/src/caffe'
-    weight_dir = home_dir+'/hpc-home/Fully-Conv-Network/Resources/FCN_models/'+ network_dir
-    snapshot_dir = glob.glob(weight_dir+'*napshot*')
-    weights = snapshot_dir[0]+'/_iter_'+ str(iteration) +'.caffemodel'
-filename, path, desc =  imp.find_module('caffe', [caffe_root+'/python/'])
+    caffe_root = home_dir + '/src/caffe'
+    weight_dir = home_dir + '/hpc-home/Fully-Conv-Network/Resources/FCN_models/' + network_dir
+    snapshot_dir = glob.glob(weight_dir + '*napshot*')
+    weights = snapshot_dir[0] + '/_iter_' + str(iteration) + '.caffemodel'
+filename, path, desc = imp.find_module('caffe', [caffe_root + '/python/'])
 caffe = imp.load_module('caffe', filename, path, desc)
 if 'g' in args.mode or 'G' in args.mode:
     caffe.set_mode_gpu()
@@ -60,18 +64,22 @@ else:
     print '-- GPU Mode Chosen --'
     print '==============='
 # caffe.set_device(1)
-import surgery, score
+import score
 
 # init
-if args.test_type=='val':
-    solver = caffe.SGDSolver(file_location+'/solver.prototxt')
-    test_set = np.loadtxt(file_location[:file_location.rfind('/')]+'/data/cs-trip/val.txt', dtype=str)
-elif args.test_type=='test':
-    solver = caffe.SGDSolver(file_location+'/solver_test.prototxt')
-    test_set = np.loadtxt(file_location[:file_location.rfind('/')]+'/data/cs-trip/test.txt', dtype=str)
-elif args.test_type=='train':
-    solver = caffe.SGDSolver(file_location+'/solver_test-trainingSet.prototxt')
-    test_set = np.loadtxt(file_location[:file_location.rfind('/')]+'/data/cs-trip/train.txt', dtype=str)
+if args.test_type == 'val':
+    solver = caffe.SGDSolver(file_location + '/solver.prototxt')
+    test_set = np.loadtxt(file_location[:file_location.rfind(
+        '/')] + '/data/cs-trip/val.txt', dtype=str)
+elif args.test_type == 'test':
+    solver = caffe.SGDSolver(file_location + '/solver_test.prototxt')
+    test_set = np.loadtxt(file_location[:file_location.rfind(
+        '/')] + '/data/cs-trip/test.txt', dtype=str)
+elif args.test_type == 'train':
+    solver = caffe.SGDSolver(
+        file_location + '/solver_test-trainingSet.prototxt')
+    test_set = np.loadtxt(file_location[:file_location.rfind(
+        '/')] + '/data/cs-trip/train.txt', dtype=str)
 else:
     print 'Incorrect test_type given {}; expecting "train", "val" or "test"'.format(args.test_type)
     raise
@@ -79,4 +87,5 @@ solver.net.copy_from(weights)
 print '-- test_type is', args.test_type, ' --'
 
 print '\n>>>> Validation <<<<\n'
-score.seg_tests(solver, file_location+'/'+args.test_type+'_images', test_set, layer='score')
+score.seg_tests(solver, file_location + '/' + args.test_type +
+                '_images', test_set, layer='score')
