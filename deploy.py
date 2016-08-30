b99663861fa25fdbe34795663b2d48992f01b836
Sean McMahon <n8307628@cl2n094.ib0.hpc.qut.edu.au>
2016-08-30 10:35:50 +1000

    adding deploy.py, will run networks without gt, just saving classifications to file. Either the images, features or both

---
+++ deploy.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python
+"""
+cstrip validate general
+
+"""
+# import caffe
+import numpy as np
+import os
+import sys
+from os.path import expanduser
+import imp
+import argparse
+import glob
+import score
+
+
+def add_slash(mystring):
+    if mystring.endswith('/'):
+        return mystring
+    else:
+        return mystring + '/'
+
+# add '../' directory to path for importing score.py, surgery.py and
+# pycaffe layer
+file_location = os.path.realpath(os.path.join(
+    os.getcwd(), os.path.dirname(__file__)))
+sys.path.append(file_location)
+home_dir = expanduser("~")
+
+# User Input
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', default='cpu')
+parser.add_argument('--iteration', default=8000)
+parser.add_argument('--test_type', default='val')
+parser.add_argument('--network_dir', default='cstrip-fcn32s-hha')
+parser.add_argument('--snapshot_filter', default='train')
+args = parser.parse_args()
+iteration = args.iteration
+network_dir = args.network_dir
+network_dir = add_slash(network_dir)  # ensure slash present
+snapshot_filter = args.snapshot_filter
+test_type = args.test_type
+print 'This is the general validation script!'
+
+# import support functions
+if 'n8307628' in home_dir:
+    caffe_root = home_dir + '/Fully-Conv-Network/Resources/caffe'
+    working_dir = home_dir + '/Fully-Conv-Network/Resources/FCN_models/' \
+        + network_dir
+elif 'sean' in home_dir:
+    caffe_root = home_dir + '/src/caffe'
+    working_dir = home_dir + \
+        '/hpc-home/Fully-Conv-Network/Resources/FCN_models/' + network_dir
+filename, path, desc = imp.find_module('caffe', [caffe_root + '/python/'])
+caffe = imp.load_module('caffe', filename, path, desc)
+if 'g' in args.mode or 'G' in args.mode:
+    caffe.set_mode_gpu()
+    print '-- GPU Mode -- {}'.format(args.mode)
+elif 'c' in args.mode or 'C' in args.mode:
+    caffe.set_mode_cpu()
+    print '-- CPU Mode -- {}'.format(args.mode)
+else:
+    caffe.set_mode_gpu()
+    print '==============='
+    print 'No Mode (CPU or GPU) Given'
+    print '-- GPU Mode Chosen --'
+    print '==============='
+# caffe.set_device(1)
+snapshot_dir = glob.glob(working_dir + '*napshot*')
+deploy_prototxt = glob.glob(working_dir + '*' + test_type + '*.prototxt')
+weights = snapshot_dir[0] + '/' + snapshot_filter + \
+    '_iter_' + str(iteration) + '.caffemodel'
+
+fcn = caffe.Net()
