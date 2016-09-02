9bfceff4aa2aebe6372a42f876b9cf21a0dd9d35
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-09-02 14:00:23 +1000

    could not fully work out the crop error, I worked out that axis=2 and offset=19 should work given the same params work with th other nets with the same deconv and color blob sizes as this one. The error stems from the use of the concat layer with is not supported with coord_map.py, I added concat as a pass through layer (which is true if concat is along depth/channel axis) but still got errors.

--- cstrip-fcn32s-color-hha-fusion/nets.py
+++ cstrip-fcn32s-color-hha-fusion/nets.py
@@ -23,19 +23,31 @@ from caffe.coord_map import crop
 
 def appendCrop(prototxt_file, bottoms):
     # append text for crop layer either at the end or before the softmax layer
+
     pass
+
 
 def fixedFusionNet(hf5_txtfile_path, batchSize):
     n = caffe.NetSpec()
     n.color_features, n.hha_features, n.label, n.in_data = layers.HDF5Data(
         batch_size=batchSize, source=hf5_txtfile_path, ntop=4)
     n.features_fused = layers.Eltwise(n.color_features, n.hha_features,
-                                      operation=params.Eltwise.SUM, coeff=[0.5, 0.5])
+                                      operation=params.Eltwise.SUM,
+                                      coeff=[0.5, 0.5])
     n.upscore = layers.Deconvolution(n.features_fused,
-                                     convolution_param=dict(num_output=2, kernel_size=64, stride=32,
+                                     convolution_param=dict(num_output=2,
+                                                            kernel_size=64,
+                                                            stride=32,
                                                             bias_term=False),
                                      param=[dict(lr_mult=0)])
-    n.score = crop(n.upscore, n.in_data)
+    try:
+        n.score = crop(n.upscore, n.in_data)
+    except:  # because crop cannot handle Concat layers
+        print '-------'
+        print 'could not initialise crop layer through coord_map.py'
+        print '-------'
+        n.score = layers.Crop(n.upscore, n.in_data,
+                              crop_param=dict(axis=2, offset=19))
     return n.to_proto()
 
 
@@ -46,18 +58,25 @@ def convFusionNet(hf5_txtfile_path, batchSize):
     n.data = layers.Concat(n.color_features, n.hha_features)
     n.score_fr = layers.Convolution(n.data, num_output=2, kernel_size=1, pad=0,
                                     weight_filler=dict(type='xavier'),
-                                    param=[dict(lr_mult=1, decay_mult=1), dict(lr_mult=2, decay_mult=0)])
+                                    param=[dict(lr_mult=1, decay_mult=1),
+                                           dict(lr_mult=2, decay_mult=0)])
     n.upscore = layers.Deconvolution(n.score_fr,
-                                     convolution_param=dict(num_output=2, kernel_size=64, stride=32,
+                                     convolution_param=dict(num_output=2,
+                                                            kernel_size=64,
+                                                            stride=32,
                                                             bias_term=False),
                                      param=[dict(lr_mult=0)])
     try:
         n.score = crop(n.upscore, n.in_data)
         n.loss = layers.SoftmaxWithLoss(n.score, n.label,
                                         loss_param=dict(normalize=False))
-    except:
-        print 'could not initialise crop layer'
+    except:  # because crop cannot handle Concat layers
+        print '-------'
+        print 'could not initialise crop layer through coord_map.py'
+        print '-------'
+        n.score = layers.Crop(n.upscore, n.in_data,
+                              crop_param=dict(axis=2, offset=19))
         n.loss = layers.SoftmaxWithLoss(
-            n.upscore, n.label, loss_param=dict(normalize=False))
+            n.score, n.label, loss_param=dict(normalize=False))
 
     return n.to_proto()
