8ea23c888d65bea5a9075029f9494f1cf79eef69
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-01-20 16:24:52 +1000

    modified paths for depth, stuff still thinking on and working out how to load pretained weights into this network because the inout is one channel

--- cstrip-fcn32s-depth/solve.py
+++ cstrip-fcn32s-depth/solve.py
@@ -28,10 +28,12 @@ print 'This is the HHA only solver!'
 if 'n8307628' in home_dir:
     caffe_root = home_dir + '/Fully-Conv-Network/Resources/caffe'
     weights = home_dir + \
-        '/Fully-Conv-Network/Resources/FCN_models/pretrained_weights/nyud-fcn32s-hha-heavy.caffemodel'
+        '/Fully-Conv-Network/Resources/FCN_models/pretrained_weights'+
+        '/nyud-fcn32s-hha-heavy.caffemodel'
 elif 'sean' in home_dir:
     caffe_root = home_dir + '/src/caffe'
-    weights = home_dir + '/hpc-home/Fully-Conv-Network/Resources/FCN_models/pretrained_weights/nyud-fcn32s-hha-heavy.caffemodel'
+    weights = home_dir + '/hpc-home/Fully-Conv-Network/Resources/'+
+    'FCN_models/pretrained_weights/nyud-fcn32s-hha-heavy.caffemodel'
 filename, path, desc = imp.find_module('caffe', [caffe_root + '/python/'])
 caffe = imp.load_module('caffe', filename, path, desc)
 if 'g' in args.mode or 'G' in args.mode:
@@ -41,8 +43,8 @@ elif 'c' in args.mode or 'C' in args.mode:
     caffe.set_mode_cpu()
     print '-- CPU Mode -- {}'.format(args.mode)
 else:
-    caffe.set_mode_gpu()
-    print '==============='
+    caffe.set_mode_gpu()\
+        print '==============='
     print 'No Mode (CPU or GPU) Given'
     print '-- GPU Mode Chosen --'
     print '==============='
@@ -51,13 +53,27 @@ import surgery
 import score
 
 # init
+print 'initialising depth only from HHA network, using weights' + \
+    ' {}'.format(weights)
+hha_net_arch = file_location[:file_location.rfind(
+    '/')] + '/cstrip-fcn32s-hha/test.prototxt'
+hha_net = caffe.Net(hha_net_arch, weights,
+                     caffe.TEST)
 solver = caffe.SGDSolver(file_location + '/solver.prototxt')
-solver.net.copy_from(weights)
+surgery.transplant(solver.net, hha_net) # copy whatever weights i can accros
 
 # surgeries
 interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
 print 'performing surgery on {}'.format(interp_layers)
 surgery.interp(solver.net, interp_layers)  # calc deconv filter weights
+# print 'copying hha params from conv1_1 (hha)  ->  conv1_1 (depth)'
+# solver.net.params['conv1_1_bgrd'][0].data[:, :3] = base_net.params[
+#     'conv1_1'][0].data
+# solver.net.params['conv1_1_bgrd'][0].data[:, 3] = np.mean(base_net.params[
+#     'conv1_1'][0].data, axis=1)
+# solver.net.params['conv1_1_bgrd'][1].data[...] = base_net.params[
+#     'conv1_1'][1].data
+# del base_net
 
 # scoring
 val = np.loadtxt(file_location[:file_location.rfind(
