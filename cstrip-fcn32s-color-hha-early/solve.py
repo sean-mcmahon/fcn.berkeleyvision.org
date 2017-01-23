a11ab6e15a92a088fa7948f04f0fd5528accdd0f
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-01-23 11:18:29 +1000

    new folder for color-hha early fusion, copied from colour-depth early fusion.

---
+++ cstrip-fcn32s-color-hha-early/solve.py
@@ -0,0 +1,101 @@
+#! /usr/bin/python
+"""
+cstrip color DEPTH
+
+"""
+# import caffe
+import numpy as np
+import os
+import sys
+from os.path import expanduser
+import imp
+import argparse
+
+# add '../' directory to path for importing score.py, surgery.py and
+# pycaffe layer
+file_location = os.path.realpath(os.path.join(
+    os.getcwd(), os.path.dirname(__file__)))
+sys.path.append(file_location[:file_location.rfind('/')])
+home_dir = expanduser("~")
+
+# User Input
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', default='CPU')
+args = parser.parse_args()
+print 'This is the colour-DEPTH solver!'
+
+# import support functions
+if 'n8307628' in home_dir:
+    caffe_root = home_dir + '/Fully-Conv-Network/Resources/caffe'
+    weights = home_dir + \
+        '/Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color/'+
+        'colorSnapshot/_iter_2000.caffemodel'
+elif 'sean' in home_dir:
+    caffe_root = home_dir + '/src/caffe'
+    weights = home_dir + '/hpc-home/Fully-Conv-Network/Resources/'+
+    'FCN_models/cstrip-fcn32s-color/colorSnapshot/_iter_2000.caffemodel'
+filename, path, desc = imp.find_module('caffe', [caffe_root + '/python/'])
+caffe = imp.load_module('caffe', filename, path, desc)
+if 'g' in args.mode or 'G' in args.mode:
+    caffe.set_mode_gpu()
+    print '-- GPU Mode -- {}'.format(args.mode)
+elif 'c' in args.mode or 'C' in args.mode:
+    caffe.set_mode_cpu()
+    print '-- CPU Mode -- {}'.format(args.mode)
+else:
+    caffe.set_mode_gpu()
+    print '==============='
+    print 'No Mode (CPU or GPU) Given'
+    print '-- GPU Mode Chosen -- {}'.format(args.mode)
+    print '==============='
+import surgery
+import score
+
+# init
+print 'Using weights from {}'.format(weights)
+base_net_arch = file_location[:file_location.rfind(
+    '/')] + '/cstrip-fcn32s-color/test.prototxt'
+base_net = caffe.Net(base_net_arch, weights,
+                     caffe.TEST)
+solver = caffe.SGDSolver(file_location + '/solver.prototxt')
+surgery.transplant(solver.net, base_net)  # copy weights to solver network
+
+# surgeries
+interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+print 'performing surgery on {}'.format(interp_layers)
+surgery.interp(solver.net, interp_layers)  # calc deconv filter weights
+# Copy weights from color network into color-depth network (I think)
+print 'copying color params from conv1_1  ->  conv1_1_bgrd'
+solver.net.params['conv1_1_bgrd'][0].data[:, :3] = base_net.params[
+    'conv1_1'][0].data
+solver.net.params['conv1_1_bgrd'][0].data[:, 3] = np.mean(base_net.params[
+    'conv1_1'][0].data, axis=1)
+solver.net.params['conv1_1_bgrd'][1].data[...] = base_net.params[
+    'conv1_1'][1].data
+del base_net
+
+# scoring
+val = np.loadtxt(file_location[:file_location.rfind('/')] +
+                 '/data/cs-trip/val.txt',
+                 dtype=str)
+
+for _ in range(50):
+    print '------------------------------'
+    print 'Running solver.step iter {}'.format(_)
+    print '------------------------------'
+    solver.step(2000)
+    # filter_1 = solver.net.params['conv1_1_bgrd'][0].data
+    # print 'layer: conv1_1_bgrd len {}, shape {}, values {}'.format(len(filter_1), np.shape(filter_1), np.unique(filter_1))
+    # filter_2 = solver.net.params['conv1_2'][0].data
+    # print 'layer: conv1_2 len {}, shape {}, values {}'.format(len(filter_2), np.shape(filter_2), np.unique(filter_2))
+    # score_fr_trip = solver.net.params['score_fr_trip'][0].data
+    # print 'layer: score_fr_trip len {}, shape {}, values {}'.format(len(score_fr_trip), np.shape(score_fr_trip), np.unique(score_fr_trip))
+    # upscore_trip = solver.net.params['upscore_trip'][0].data
+    # print 'layer: upscore_trip len {}, shape {}, values {}'.format(len(upscore_trip), np.shape(upscore_trip), np.unique(upscore_trip))
+    # break
+
+    # if getting issues on HPC try
+    # export MKL_CBWR=AUTO
+    # and 'export CUDA_VISIBLE_DEVICES=1'
+    # print '\n>>>> Validation <<<<\n'
+    # score.seg_tests(solver, False, val, layer='score')
---
+++ cstrip-fcn32s-color-hha-early/test_fcn.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -l
+#:
+#PBS -N validate_FCN
+#PBS -l ngpus=1
+#PBS -l ncpus=1
+#PBS -l mem=4GB
+#PBS -l walltime=4:00:00
+
+module load python
+module load caffe
+module load cuda
+
+USEGPU='true'
+if [[ $(lsb_release -si) == *"SUSE LINUX"* ]]; then
+    # On HPC (probably)
+
+    # Old GPU ID method only works on nodes with 2x GPUs
+    # GPU_ID=$(nvidia-smi | awk '{ if(NR==19) if($2>0) print 0; else print 1 }')
+
+    # New GPU ID method works on nodes with 1 or more GPUs
+    PROCESSES=$((nvidia-smi -q -d pids | grep Processes) | awk '{printf "%sHereBro ",$3}')
+    ind=0
+    GPU_ID=-1
+    for process in $PROCESSES; do
+        echo $process
+        if [[ "$process" == "NoneHereBro" ]]; then
+            GPU_ID=$ind
+            break
+        fi
+        ind=$[ind + 1]
+    done
+else
+    # Not on HPC (probably)
+    GPU_ID=$(nvidia-smi --list-gpus | awk '{NR;}END {print NR-1}') # Grabs number of GPUS
+fi
+
+if [ $USEGPU == 'true' ]; then
+    echo "Using gpu: $GPU_ID"
+    export CUDA_VISIBLE_DEVICES=$GPU_ID
+    gpu=$GPU_ID
+else
+    echo "Using cpu"
+    gpu=-1
+fi
+
+hpc_dir='/home/n8307628'
+local_dir='/home/sean'
+if [[ -d $local_dir ]]; then
+  working_dir=$local_dir'/hpc-home/Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color-d'
+  python_script=$working_dir'/validate.py'
+elif [[ -d $hpc_dir ]]; then
+  working_dir=$hpc_dir'/Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color-d'
+  python_script=$working_dir'/validate.py'
+  # Because using MKL Blas on HPC
+  export MKL_CBWR=AUTO
+else
+  echo "No directory found..."
+fi
+
+set_mode="$1"
+if [[ -z "$set_mode" ]]; then
+  set_mode='gpu'
+fi
+split="$2"
+if [[ -z "$split" ]]; then
+  split='val'
+fi
+snapshot_iter="$3"
+if [[ -z "$snapshot_iter" ]]; then
+  snapshot_iter='24000'
+fi
+snapshot_filter_="$4"
+if [[ -z "$snapshot_filter_" ]]; then
+  snapshot_filter_='colorInit'
+fi
+
+# current_date=`date +%Y-%m-%d_%H-%M-%S`
+log_filename=$working_dir'/logs/'$split'_dataset_snapshot_'$snapshot_filter_'_'$snapshot_iter'_results.log'
+
+python $python_script --mode $set_mode --test_type $split --iteration $snapshot_iter --snapshot_filter $snapshot_filter_ 2>&1 | tee $log_filename
