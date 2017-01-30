c5a811383591503253fb8aaea71ff731e7372119
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-01-30 13:20:47 +1000

    updated log file save names

--- create_trainval_vis.sh
+++ create_trainval_vis.sh
@@ -3,7 +3,7 @@
 #PBS -N val_nets
 #PBS -l ngpus=1
 #PBS -l ncpus=1
-#PBS -l mem=4GB
+#PBS -l mem=16GB
 #PBS -l walltime=8:00:00
 #PBS -l gputype=K40
 
@@ -70,24 +70,24 @@ fi
 
 network_='cstrip-fcn32s-depth'
 snapshot_='negOneNull_mean_sub'
-log_filename=$working_dir'/'$network_'/logs/trainval_acc_vis.log'
+log_filename=$working_dir'/'$network_'/logs/trainval_acc_vis_'$snapshot_'.log'
 python $val_script --test_type 'trainval' --snapshot_filter $snapshot_ --network_dir $network_ >> $log_filename 2>&1
 echo 'logfilename '$log_filename
 
 network_='cstrip-fcn32s-color-d'
 snapshot_='colorInit_5xLR'
-log_filename=$working_dir'/'$network_'/logs/trainval_acc_vis.log'
+log_filename=$working_dir'/'$network_'/logs/trainval_acc_vis_'$snapshot_'.log'
 python $val_script --test_type 'trainval' --snapshot_filter $snapshot_ --network_dir $network_ >> $log_filename 2>&1
 echo 'logfilename '$log_filename
 
 network_='cstrip-fcn32s-hha'
 snapshot_='secondTrain_lowerLR'
-log_filename=$working_dir'/'$network_'/logs/trainval_hha_vis.log'
+log_filename=$working_dir'/'$network_'/logs/trainval_hha_vis_'$snapshot_'.log'
 python $val_script --test_type 'trainval' --snapshot_filter $snapshot_ --network_dir $network_ >> $log_filename 2>&1
 echo 'logfilename '$log_filename
 
 network_='cstrip-fcn32s-color-hha-early'
 snapshot_='colorHhaInit_5xLR'
-log_filename=$working_dir'/'$network_'/logs/trainval_rgbhha_vis.log'
+log_filename=$working_dir'/'$network_'/logs/trainval_rgbhha_vis_'$snapshot_'.log'
 python $val_script --test_type 'trainval' --snapshot_filter $snapshot_ --network_dir $network_ >> $log_filename 2>&1
 echo 'logfilename '$log_filename
