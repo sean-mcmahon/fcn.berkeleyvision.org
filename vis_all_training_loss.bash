9ad95b8d6dc2707552b922d297eeb0ddd2484e7c
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-01-30 16:55:53 +1000

    a quick way to run all loss visualisations

---
+++ vis_all_training_loss.bash
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+hpc_dir='/home/n8307628'
+local_dir='/home/sean'
+if [[ -d $local_dir ]]; then
+  working_dir=$local_dir'/hpc-home/Fully-Conv-Network/Resources/FCN_models'
+  vis_script=$working_dir'/vis_finetune.py'
+elif [[ -d $hpc_dir ]]; then
+  working_dir=$hpc_dir'/Fully-Conv-Network/Resources/FCN_models'
+  vis_script=$working_dir'/vis_finetune.py'
+else
+  echo "No directory found..."
+fi
+
+color_logpath=$working_dir'/cstrip-fcn32s-color/logs/FCNcolor_train2016-08-22_12-20-47.log'
+colorD_logpath=$working_dir'/cstrip-fcn32s-color-d/logs/FCNcolorDepth_train2016-08-25_10-43-23.log'
+depth_logpath=$working_dir'/cstrip-fcn32s-depth/logs/FCNdepth_train2017-01-23_13-23-32_negOneNull.log'
+hha_logpath=$working_dir'/cstrip-fcn32s-hha/logs/FCNhha_train2016-08-30_21-50-25.log'
+colorHHA_logpath=$working_dir'/cstrip-fcn32s-color-hha-early/logs/FCNcolorHhaEarly_train2017-01-23_16-31-20_pretrainHha.log'
+
+python $vis_script $color_logpath $colorD_logpath $depth_logpath $hha_logpath $colorHHA_logpath
