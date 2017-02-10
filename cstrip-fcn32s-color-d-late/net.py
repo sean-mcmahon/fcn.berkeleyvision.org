efdc94fed3ed932d60228d88b5ad1964588c38e7
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-02-10 13:57:59 +1000

    created files for late fusion of colour and depth, using late network fusion. These are just copies from color-hha2-late

---
+++ cstrip-fcn32s-color-d-late/net.py
@@ -0,0 +1,264 @@
+import caffe
+from caffe import layers as L, params as P
+from caffe.coord_map import crop
+
+
+def conv_relu(bottom, nout, ks=3, stride=1, pad=1):
+    conv = L.Convolution(bottom, kernel_size=ks, stride=stride,
+                         num_output=nout, pad=pad,
+                         param=[dict(lr_mult=1, decay_mult=1), dict(lr_mult=2, decay_mult=0)])
+    return conv, L.ReLU(conv, in_place=True)
+
+
+def max_pool(bottom, ks=2, stride=2):
+    return L.Pooling(bottom, pool=P.Pooling.MAX, kernel_size=ks, stride=stride)
+
+
+def modality_fcn(net_spec, data, modality):
+    n = net_spec
+    # the base net
+    n['conv1_1' + modality], n['relu1_1' + modality] = conv_relu(n[data], 64,
+                                                                 pad=100)
+    n['conv1_2' + modality], n['relu1_2' + modality] = conv_relu(n['relu1_1' +
+                                                                   modality], 64)
+    n['pool1' + modality] = max_pool(n['relu1_2' + modality])
+
+    n['conv2_1' + modality], n['relu2_1' + modality] = conv_relu(n['pool1' +
+                                                                   modality], 128)
+    n['conv2_2' + modality], n['relu2_2' + modality] = conv_relu(n['relu2_1' +
+                                                                   modality], 128)
+    n['pool2' + modality] = max_pool(n['relu2_2' + modality])
+
+    n['conv3_1' + modality], n['relu3_1' + modality] = conv_relu(n['pool2' +
+                                                                   modality], 256)
+    n['conv3_2' + modality], n['relu3_2' + modality] = conv_relu(n['relu3_1' +
+                                                                   modality], 256)
+    n['conv3_3' + modality], n['relu3_3' + modality] = conv_relu(n['relu3_2' +
+                                                                   modality], 256)
+    n['pool3' + modality] = max_pool(n['relu3_3' + modality])
+
+    n['conv4_1' + modality], n['relu4_1' + modality] = conv_relu(n['pool3' +
+                                                                   modality], 512)
+    n['conv4_2' + modality], n['relu4_2' + modality] = conv_relu(n['relu4_1' +
+                                                                   modality], 512)
+    n['conv4_3' + modality], n['relu4_3' + modality] = conv_relu(n['relu4_2' +
+                                                                   modality], 512)
+    n['pool4' + modality] = max_pool(n['relu4_3' + modality])
+
+    n['conv5_1' + modality], n['relu5_1' + modality] = conv_relu(n['pool4' +
+                                                                   modality], 512)
+    n['conv5_2' + modality], n['relu5_2' + modality] = conv_relu(n['relu5_1' +
+                                                                   modality], 512)
+    n['conv5_3' + modality], n['relu5_3' + modality] = conv_relu(n['relu5_2' +
+                                                                   modality], 512)
+    n['pool5' + modality] = max_pool(n['relu5_3' + modality])
+
+    # fully conv
+    n['fc6' + modality], n['relu6' + modality] = conv_relu(
+        n['pool5' + modality], 4096, ks=7, pad=0)
+    n['drop6' + modality] = L.Dropout(
+        n['relu6' + modality], dropout_ratio=0.5, in_place=True)
+    n['fc7' + modality], n['relu7' + modality] = conv_relu(
+        n['drop6' + modality], 4096, ks=1, pad=0)
+    n['drop7' + modality] = L.Dropout(
+        n['relu7' + modality], dropout_ratio=0.5, in_place=True)
+    n['score_fr_trip' + modality] = L.Convolution(
+        n['drop7' + modality], num_output=2, kernel_size=1, pad=0,
+        param=[dict(lr_mult=1, decay_mult=1), dict(lr_mult=2, decay_mult=0)],
+        weight_filler=dict(type='msra'))
+    return n
+
+
+def fcn(split, tops):
+    n = caffe.NetSpec()
+    n.color, n.hha2, n.label = L.Python(module='cs_trip_layers',
+                                        layer='CStripSegDataLayer', ntop=3,
+                                        param_str=str(dict(
+                                            cstrip_dir='/Construction_Site/' +
+                                            'Springfield/12Aug16/K2', split=split,
+                                            tops=tops, seed=1337)))
+    n = modality_fcn(n, 'color', 'color')
+    n = modality_fcn(n, 'hha2', 'hha2')
+    n.score_fused = L.Eltwise(n.score_fr_tripcolor, n.score_fr_triphha2,
+                              operation=P.Eltwise.SUM, coeff=[0.5, 0.5])
+    n.upscore = L.Deconvolution(n.score_fused,
+                                convolution_param=dict(num_output=2,
+                                                       kernel_size=64,
+                                                       stride=32,
+                                                       bias_term=False),
+                                param=[dict(lr_mult=0)])
+    n.score = crop(n.upscore, n.color)
+    n.loss = L.SoftmaxWithLoss(n.score, n.label,
+                               loss_param=dict(normalize=False))
+    return n.to_proto()
+
+def convFusionfcn(split, tops):
+    n = caffe.NetSpec()
+    n.color, n.hha2, n.label = L.Python(module='cs_trip_layers',
+                                        layer='CStripSegDataLayer', ntop=3,
+                                        param_str=str(dict(
+                                            cstrip_dir='/Construction_Site/' +
+                                            'Springfield/12Aug16/K2', split=split,
+                                            tops=tops, seed=1337)))
+    n = modality_fcn(n, 'color', 'color')
+    n = modality_fcn(n, 'hha2', 'hha2')
+
+    n.score_concat = L.Concat(n.fc7color, n.fc7hha2)
+    n.conv_fusion1 = L.Convolution(
+        n.score_concat, num_output=4096, kernel_size=1, pad=0,
+        param=[dict(lr_mult=4, decay_mult=1), dict(lr_mult=8, decay_mult=0)],
+        weight_filler=dict(type='msra'))
+    n.relu_fusion1 = L.ReLU(n.conv_fusion1, in_place=True)
+    n.conv_fusion2 = L.Convolution(
+        n.conv_fusion1, num_output=2, kernel_size=1, pad=0,
+        param=[dict(lr_mult=4, decay_mult=1), dict(lr_mult=8, decay_mult=0)],
+        weight_filler=dict(type='msra'))
+    n.upscore_fused = L.Deconvolution(n.conv_fusion2,
+                                convolution_param=dict(num_output=2,
+                                                       kernel_size=64,
+                                                       stride=32,
+                                                       bias_term=False),
+                                param=[dict(lr_mult=0)])
+    n.score_fused = crop(n.upscore_fused, n.color)
+    n.loss = L.SoftmaxWithLoss(n.score_fused, n.label,
+                               loss_param=dict(normalize=False))
+    return n.to_proto()
+
+def mixfcn(split, tops):
+    n = caffe.NetSpec()
+    n.color, n.hha2, n.label = L.Python(module='cs_trip_layers',
+                                        layer='CStripSegDataLayer', ntop=3,
+                                        param_str=str(dict(
+                                            cstrip_dir='/Construction_Site/' +
+                                            'Springfield/12Aug16/K2', split=split,
+                                            tops=tops, seed=1337)))
+    n = modality_fcn(n, 'color', 'color')
+    n = modality_fcn(n, 'hha2', 'hha2')
+    # find max trip or non trip confidences, cannot use Argmax (no backprop)
+    # using eltwise max with split instead
+    n.score_colora, n.score_colorb = L.Slice(
+        n.score_fr_tripcolor, ntop=2,  slice_param=dict(axis=1))
+    n.maxcolor = L.Eltwise(n.score_colora, n.score_colorb,
+                           operation=P.Eltwise.MAX)
+    n.score_HHA2a, n.score_HHA2b = L.Slice(
+        n.score_fr_triphha2, ntop=2,  slice_param=dict(axis=1))
+    n.maxhha2 = L.Eltwise(n.score_HHA2a, n.score_HHA2b,
+                          operation=P.Eltwise.MAX)
+    # concatinate together and softmax for 'probabilites'
+    n.maxConcat = L.Concat(n.maxcolor, n.maxhha2, concat_param=dict(axis=1))
+    n.maxSoft = L.Softmax(n.maxConcat)
+    # separate color and hha using slice layer
+    n.probColor, n.probHHA2 = L.Slice(
+        n.maxSoft, ntop=2,  slice_param=dict(axis=1))
+    # duplicate probabilies using concat layer over dim1 for mulitplication
+    n.repProbColor = L.Concat(n.probColor, n.probColor)
+    n.repProbHHA2 = L.Concat(n.probHHA2, n.probHHA2)
+    # multiply the 'probabilies' with the color and hha scores
+    n.weightedColor = L.Eltwise(n.score_fr_tripcolor, n.repProbColor,
+                                operation=P.Eltwise.PROD)
+    n.weightedHHA2 = L.Eltwise(n.score_fr_triphha2, n.repProbHHA2,
+                               operation=P.Eltwise.PROD)
+    # combine the prob scores with eltwise summation
+    n.score_fused = L.Eltwise(n.weightedColor, n.weightedHHA2,
+                              operation=P.Eltwise.SUM, coeff=[1, 1])
+    n.upscore = L.Deconvolution(n.score_fused,
+                                convolution_param=dict(num_output=2,
+                                                       kernel_size=64,
+                                                       stride=32,
+                                                       bias_term=False),
+                                param=[dict(lr_mult=0)])
+    n.score = crop(n.upscore, n.color)
+    n.loss = L.SoftmaxWithLoss(n.score, n.label,
+                               loss_param=dict(normalize=False))
+    return n.to_proto()
+
+
+def lateMixfcn(split, tops):
+    n = caffe.NetSpec()
+    n.color, n.hha2, n.label = L.Python(module='cs_trip_layers',
+                                        layer='CStripSegDataLayer', ntop=3,
+                                        param_str=str(dict(
+                                            cstrip_dir='/Construction_Site/' +
+                                            'Springfield/12Aug16/K2', split=split,
+                                            tops=tops, seed=1337)))
+    n = modality_fcn(n, 'color', 'color')
+    n = modality_fcn(n, 'hha2', 'hha2')
+
+    n.upscore_color = L.Deconvolution(n.score_fr_tripNEWcolor,
+                                      convolution_param=dict(num_output=2,
+                                                             kernel_size=64,
+                                                             stride=32,
+                                                             bias_term=False),
+                                      param=[dict(lr_mult=0)])
+    n.score_color = crop(n.upscore_color, n.color)
+    n.upscore_hha2 = L.Deconvolution(n.score_fr_tripNEWhha2,
+                                     convolution_param=dict(num_output=2,
+                                                            kernel_size=64,
+                                                            stride=32,
+                                                            bias_term=False),
+                                     param=[dict(lr_mult=0)])
+    n.score_hha2 = crop(n.upscore_hha2, n.hha2)
+
+    # find max trip or non trip confidences, cannot use Argmax (no backprop)
+    # using eltwise max with split instead
+    n.score_colora, n.score_colorb = L.Slice(
+        n.score_color, ntop=2,  slice_param=dict(axis=1))
+    n.maxcolor = L.Eltwise(n.score_colora, n.score_colorb,
+                           operation=P.Eltwise.MAX)
+    n.score_HHA2a, n.score_HHA2b = L.Slice(
+        n.score_hha2, ntop=2,  slice_param=dict(axis=1))
+    n.maxhha2 = L.Eltwise(n.score_HHA2a, n.score_HHA2b,
+                          operation=P.Eltwise.MAX)
+    # concatinate together and softmax for 'probabilites'
+    n.maxConcat = L.Concat(n.maxcolor, n.maxhha2, concat_param=dict(axis=1))
+    n.maxSoft = L.Softmax(n.maxConcat)
+    # separate color and hha using slice layer
+    n.probColor, n.probHHA2 = L.Slice(
+        n.maxSoft, ntop=2,  slice_param=dict(axis=1))
+    # duplicate probabilies using concat layer over dim1 for mulitplication
+    n.repProbColor = L.Concat(n.probColor, n.probColor)
+    n.repProbHHA2 = L.Concat(n.probHHA2, n.probHHA2)
+    # multiply the 'probabilies' with the color and hha scores
+    n.weightedColor = L.Eltwise(n.score_color, n.repProbColor,
+                                operation=P.Eltwise.PROD)
+    n.weightedHHA2 = L.Eltwise(n.score_hha2, n.repProbHHA2,
+                               operation=P.Eltwise.PROD)
+    # combine the prob scores with eltwise summation
+    n.score_fused = L.Eltwise(n.weightedColor, n.weightedHHA2,
+                              operation=P.Eltwise.SUM, coeff=[1, 1])
+    n.loss = L.SoftmaxWithLoss(n.score_fused, n.label,
+                               loss_param=dict(normalize=False))
+    return n.to_proto()
+
+
+def make_net():
+    tops = ['color', 'hha2', 'label']
+    # with open('trainval.prototxt', 'w') as f:
+    #     f.write(str(fcn('train', tops)))
+    # with open('val.prototxt', 'w') as f:
+    #     f.write(str(fcn('val', tops)))
+    # with open('test.prototxt', 'w') as f:
+    #     f.write(str(fcn('test', tops)))
+    #
+    # with open('trainval_mix.prototxt', 'w') as f:
+    #     f.write(str(mixfcn('train', tops)))
+    # with open('val_mix.prototxt', 'w') as f:
+    #     f.write(str(mixfcn('val', tops)))
+    # with open('test_mix.prototxt', 'w') as f:
+    #     f.write(str(mixfcn('test', tops)))
+
+    # with open('trainval_latemix2.prototxt', 'w') as f:
+    #     f.write(str(lateMixfcn('train', tops)))
+    # with open('val_latemix2.prototxt', 'w') as f:
+    #     f.write(str(lateMixfcn('val', tops)))
+    # with open('test_latemix2.prototxt', 'w') as f:
+    #     f.write(str(lateMixfcn('test', tops)))
+
+    with open('trainval_conv.prototxt', 'w') as f:
+        f.write(str(convFusionfcn('train', tops)))
+    with open('val_conv.prototxt', 'w') as f:
+        f.write(str(convFusionfcn('val', tops)))
+    with open('test_conv.prototxt', 'w') as f:
+        f.write(str(convFusionfcn('test', tops)))
+if __name__ == '__main__':
+    make_net()
---
+++ cstrip-fcn32s-color-d-late/solve.py
@@ -0,0 +1,139 @@
+#! /usr/bin/python
+"""
+cstrip Color and HHA2 Eltwise late fusion
+
+"""
+# import caffe
+import numpy as np
+import os
+import sys
+from os.path import expanduser
+import imp
+import argparse
+
+
+def fusion_solver(train_net_path, test_net_path, file_location):
+    s = caffe_pb2.SolverParameter()
+    s.train_net = train_net_path
+    s.test_net.append(test_net_path)
+    s.test_interval = 999999999  # do not invoke tests here
+    s.test_iter.append(654)
+    s.max_iter = 300000
+    s.base_lr = 5e-13
+    s.lr_policy = 'fixed'
+    s.gamma = 0.1
+    s.average_loss = 20
+    s.momentum = 0.99
+    s.iter_size = 1
+    s.weight_decay = 0.0005
+    s.display = 20
+    s.snapshot = 1000
+
+    snapshot_dir = file_location + '/fusionSnapshot/secondTrain'
+    if not os.path.isdir(snapshot_dir):
+        os.mkdir(snapshot_dir)
+    s.snapshot_prefix = snapshot_dir
+    s.test_initialization = False
+    return s
+
+# add '../' directory to path for importing score.py, surgery.py and
+# pycaffe layer
+file_location = os.path.realpath(os.path.join(
+    os.getcwd(), os.path.dirname(__file__)))
+sys.path.append(file_location[:file_location.rfind('/')])
+file_parent_dir = file_location[:file_location.rfind('/')]
+home_dir = expanduser("~")
+
+# User Input
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', default='GPU')
+parser.add_argument('--fusion_type', default='mixDCNN')
+args = parser.parse_args()
+print 'This is the colour-HHA2 summation solver!'
+fusion_type = args.fusion_type
+
+# import support functions
+if 'n8307628' in home_dir:
+    caffe_root = home_dir + '/Fully-Conv-Network/Resources/caffe'
+elif 'sean' in home_dir:
+    caffe_root = home_dir + '/src/caffe'
+filename, path, desc = imp.find_module('caffe', [caffe_root + '/python/'])
+caffe = imp.load_module('caffe', filename, path, desc)
+if 'g' in args.mode or 'G' in args.mode:
+    caffe.set_mode_gpu()
+    print '-- GPU Mode -- {}'.format(args.mode)
+elif 'c' in args.mode or 'C' in args.mode:
+    caffe.set_mode_cpu()
+    print '-- CPU Mode -- {}'.format(args.mode)
+else:
+    caffe.set_mode_gpu()
+    print '==============='
+    print 'No Mode (CPU or GPU) Given'
+    print '-- GPU Mode Chosen --'
+    print '==============='
+# caffe.set_device(1)
+import surgery
+import score
+
+color_weights = file_parent_dir + \
+    '/cstrip-fcn32s-color/colorSnapshot/_iter_2000.caffemodel'
+color_proto = file_parent_dir + '/cstrip-fcn32s-color/test.prototxt'
+hha2_weights = file_parent_dir + \
+    '/cstrip-fcn32s-hha2/HHA2snapshot/secondTrain_lowerLR_iter_2000.caffemodel'
+hha2_proto = file_parent_dir + '/cstrip-fcn32s-hha2/test.prototxt'
+if fusion_type == 'sum' or fusion_type == 'Sum' or fusion_type == 'SUM':
+    print '------\n Loading sum fusion approach \n------'
+    score_layer = 'score'
+    solver = caffe.SGDSolver(file_location + '/solver.prototxt')
+elif fusion_type == 'mixDCNN' or fusion_type == 'mixdcnn':
+    print '------\n Loading mixDCNN fusion approach \n------'
+    score_layer = 'score'
+    solver = caffe.SGDSolver(file_location + '/solver_mix.prototxt')
+elif fusion_type == 'latemixDCNN' or fusion_type == 'latemixdcnn'  \
+        or fusion_type == 'lateMixDCNN' or fusion_type == 'latemix'  \
+        or fusion_type == 'lateMix':
+    score_layer = 'score_fused'
+    print '------\n Loading lateMixDCNN fusion approach \n------'
+    solver = caffe.SGDSolver(file_location + '/solver_latemix.prototxt')
+elif fusion_type == 'conv' or fusion_type == 'Conv'  \
+        or fusion_type == 'ConvFusion' or fusion_type == 'convFusion':
+    score_layer = 'score_fused'
+    print '------\n Loading convolutional layer fusion approach \n------'
+    solver = caffe.SGDSolver(file_location + '/solver_conv.prototxt')
+else:
+    print 'unrecognised or no fusion approach specified,'
+    raise
+
+# surgeries
+color_net = caffe.Net(color_proto, color_weights, caffe.TEST)
+surgery.transplant(solver.net, color_net, suffix='color')
+del color_net
+
+hha2_net = caffe.Net(hha2_proto, hha2_weights, caffe.TEST)
+surgery.transplant(solver.net, hha2_net, suffix='hha2')
+del hha2_net
+
+interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+print 'performing surgery on {}'.format(interp_layers)
+surgery.interp(solver.net, interp_layers)
+
+# scoring
+val = np.loadtxt(
+    file_location[:file_location.rfind('/')] + '/data/cs-trip/val.txt',
+    dtype=str)
+# train = np.loadtxt(
+#     file_location[:file_location.rfind('/')] + '/data/cs-trip/train.txt',
+#     dtype=str)
+score.seg_loss_tests(solver, val, layer=score_layer)
+
+for _ in range(25):
+    print '------------------------------'
+    print 'Running solver.step iter {}'.format(_)
+    print '------------------------------'
+    solver.step(250)
+    # test on validation
+    score.seg_loss_tests(solver, val, layer=score_layer)
+    # test on training set
+    # score.seg_loss(solver.net, solver.iter, train, test_type='training',
+    #                calc_hist=True, layer=score_layer)
+print '\n(python) color-hha2 fusion, fusion_type', fusion_type
