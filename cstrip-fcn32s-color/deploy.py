3586557008597cb87c92a5a8e1b9f6862b9ffa53
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-03-08 11:59:16 +1000

    added some visualisation for hpc, and loop over all test images

--- cstrip-fcn32s-color/deploy.py
+++ cstrip-fcn32s-color/deploy.py
@@ -16,17 +16,18 @@ else:
 
 home_dir = os.path.expanduser("~")
 if 'n8307628' in home_dir:
-    caffe_root = os.path.join(home_dir,'Fully-Conv-Network/Resources/caffe')
+    caffe_root = os.path.join(home_dir, 'Fully-Conv-Network/Resources/caffe')
     base_dir = '/home/n8307628/'
 elif 'sean' in home_dir:
-    caffe_root = os.path.join(home_dir,'src/caffe')
+    caffe_root = os.path.join(home_dir, 'src/caffe')
     base_dir = '/home/sean/hpc-home/'
 else:
     print 'unknown directory'
     raise
 print 'loading caffe from ', caffe_root
-filename, path, desc =  imp.find_module('caffe', [caffe_root+'/python/'])
+filename, path, desc = imp.find_module('caffe', [caffe_root + '/python/'])
 caffe = imp.load_module('caffe', filename, path, desc)
+
 
 def prepImage(img):
     # perform image preprocessing for CNN with caffe
@@ -44,7 +45,7 @@ def prepImage(img):
     return n_img
 
 
-def deploy(net, data, visualise=True):
+def deploy(net, data, visualise=True, image_name='overlay_image'):
     in_img = prepImage(data)
     # reshape input for any sized image (data blob is N x C x H x W)
     net.blobs['data'].reshape(1, *in_img.shape)
@@ -65,11 +66,17 @@ def deploy(net, data, visualise=True):
         im = Image.fromarray(out_img, mode='P')
         overlay = Image.blend(colorIm.convert(
             "RGBA"), im.convert("RGBA"), 0.7)
-        cv_overlay = np.array(overlay)
-        cv2.imshow('Image', out_img)
-        cv2.waitKey(0)
-        cv2.imshow('Image2', cv_overlay)
-        cv2.waitKey(0)
+        basename = os.path.splitext(os.path.basename(image_name))[0]
+
+        if 'n8307628' in home_dir:
+            overlay.save(os.path.join(home_dir, 'baysian_cnn_images',
+                                      basename + '_overlay.png'))
+        else:
+            cv_overlay = np.array(overlay)
+            cv2.imshow('Image', out_img)
+            cv2.waitKey(0)
+            cv2.imshow('Image2', cv_overlay)
+            cv2.waitKey(0)
         # convert from rgb to bgr - needed?
         # cv_overlay = cv_overlay[:, :, ::-1].copy()
     return net
@@ -78,19 +85,20 @@ if __name__ == '__main__':
     file_location = os.path.realpath(
         os.path.join(os.getcwd(), os.path.dirname(__file__)))
     # load image
-    test_dir = os.path.join(base_dir, 'Construction_Site/Springfield/12Aug16/K2/2016-08-12-10-09-26_groundfloorCarPark/labelled_colour/')
+    test_dir = os.path.join(
+        base_dir, 'Construction_Site/Springfield/12Aug16/K2/2016-08-12-10-09-26_groundfloorCarPark/labelled_colour/')
     img_names = glob.glob(os.path.join(test_dir, '*.png'))
     # initialise network
-    arch = os.path.join(file_location,'deploy_col.prototxt')
+    arch = os.path.join(file_location, 'deploy_col.prototxt')
     weights = os.path.join(
         base_dir, 'Fully-Conv-Network/Resources/FCN_models/cstrip-fcn32s-color/colorSnapshot/_iter_6000.caffemodel')
     caffe.set_mode_gpu()
     net = caffe.Net(arch, weights, caffe.TEST)
     num_images = len(img_names)
     for count, name in enumerate(img_names):
-        print 'loading image ', name
         image = Image.open(name)
+        # print 'loaded image ', os.path.basename(name), ' has shape ', np.shape(image)
         # forward pass
-        print 'foward pass ', count, ' of ', num_images
-        net = deploy(net, image, visualise=True)
-        break
+        print 'foward pass ', count+1, ' of ', num_images
+        net = deploy(net, image, visualise=True, image_name=name)
+        # break
