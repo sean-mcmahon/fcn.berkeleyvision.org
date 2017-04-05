a453943094e359210d5a8a5e1cae9f0ff273362b
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-04-05 13:17:20 +1000

    removed nyud stuff, Ive never used it. And removed hha stuff as hha2 has replaced it

--- cstrip-fcn32s-hha/wait_to_submit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-timeatstart=`date +%Y-%m-%d_%H-%M-%S`
-echo "waiting to start training hha FCN at $timeatstart..."
-sleep 4h
-submit_date=`date +%Y-%m-%d_%H-%M-%S`
-echo "submitting job at $submit_date"
-qsub train_bash
