a5eb3534d77622c169864b2517341dc77784a094
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-04-21 15:25:03 +1000

    ignore the live versions of worke and solver any they are for stuff running on hpc. run_master now creates these

--- run_master.bash
+++ run_master.bash
@@ -8,8 +8,12 @@ module load python/2.7.11-foss-2016a
 
 hpc_dir='/home/n8307628'
 working_dir=$hpc_dir'/Fully-Conv-Network/Resources/FCN_paramsearch/'
+local_working_dir='/home/sean/Dropbox/Uni/Code/FCN_models/'
 python_script=$working_dir'master.py'
 # Because using MKL Blas on HPC
 export MKL_CBWR=AUTO
 
-python $python_script
+cp $working_dir'worker.bash' $working_dir'worker_live.bash'
+cp $working_dir'solve_any.py' $working_dir'solve_any_live.py'
+
+# python $python_script
