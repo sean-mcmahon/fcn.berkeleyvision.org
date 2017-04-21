997c35241af6b4feaed802b32c454ea1049f9ad7
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-04-21 12:34:00 +1000

    slowly building up, but dealing with regex is painful

--- clean_logs.py
+++ clean_logs.py
@@ -7,10 +7,36 @@ By Sean McMahon
 """
 
 import numpy as np
-import glob
-import os.path
-import os.walk
+import fnmatch
+import os
 import click
+import re
 
 if __name__ == '__main__':
     # walk through directory and find .log files.
+    dir_ = '/home/sean/hpc-home/Fully-Conv-Network/Resources/' + \
+        'FCN_paramsearch/rgb_workers/rgb_1_23/'
+
+    folders = os.walk(dir_)
+    logfile_names = []
+    for root, dirs, files in folders:
+        for f in fnmatch.filter(files, '*.log'):
+            logfile_names.append(os.path.join(root, f))
+            print os.path.join(root, f)
+    print '-' * 20
+    for logfile_name in logfile_names:
+        # regex matching!
+        with open(logfile_name, 'r') as f:
+            logfile = f.read()
+        io_pattern = '[^\n]I0'
+        # have read the entire logfile into memory as a string
+        # find 'I0's not at the start of a line
+        idx_match = re.finditer(io_pattern, logfile)
+        for match in idx_match:
+            print match
+
+        # find all 'I0's
+        # find those without '\n' before it?
+
+        # copy and remove text before matched 'I0' and then add '\n' at start
+        # put copied text (remove \n) at start of next line after 'I0's
