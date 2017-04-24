e8493dc5139d7d78b033d6c41d776cf59f7cd632
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-04-24 12:34:41 +1000

    written code to test conditions of networks.py

--- networks.py
+++ networks.py
@@ -35,6 +35,16 @@ def createNet(split, net_type='rgb', f_multi=5, dropout_prob=0.5,
                                   conv1_1_lr_multi=conv11_multi,
                                   final_multi=f_multi, dropout_prob=dropout_prob,
                                   freeze=freeze)
+    elif net_type == 'rgbd_conv' or net_type == 'RGBD_conv':
+        tops = ['color', 'depth', 'label']
+        net = net_archs.fcn_conv(split, tops, engineNum=engine,
+                                 final_multi=f_multi, dropout_prob=dropout_prob,
+                                 freeze=freeze)
+    elif net_type == 'rgbhha2_conv' or net_type == 'rgbHHA2_conv':
+        tops = ['color', 'hha2', 'label']
+        net = net_archs.fcn_conv(split, tops, engineNum=engine,
+                                 final_multi=f_multi, dropout_prob=dropout_prob,
+                                 freeze=freeze)
     else:
         raise(Exception('net_type "' + net_type +
                         '" unrecognised, create case for new network here.'))
@@ -46,16 +56,13 @@ def createNet(split, net_type='rgb', f_multi=5, dropout_prob=0.5,
 def print_net(path, split='test', net_type='rgb'):
     if net_type == 'rgb' or net_type == 'RGB':
         tops = ['color', 'label']
-        with open(os.path.join(path, split + '.prototxt'), 'w') as f:
-            f.write(str(net_archs.fcn(split, tops).to_proto()))
+        net = net_archs.fcn(split, tops)
     elif net_type == 'depth' or net_type == 'Depth':
         tops = ['depth', 'label']
-        with open(os.path.join(path, split + '.prototxt'), 'w') as f:
-            f.write(str(net_archs.fcn(split, tops).to_proto()))
+        net = net_archs.fcn(split, tops)
     elif net_type == 'hha2' or net_type == 'HHA2':
         tops = ['hha2', 'label']
-        with open(os.path.join(path, split + '.prototxt'), 'w') as f:
-            f.write(str(net_archs.fcn(split, tops).to_proto()))
+        net = net_archs.fcn(split, tops)
     elif '_early' in net_type:
         if 'd' in net_type or 'D' in net_type:
             tops = ['color', 'depth', 'label']
@@ -63,14 +70,23 @@ def print_net(path, split='test', net_type='rgb'):
             tops = ['color', 'hha2', 'label']
         else:
             raise(Exception('Unkown modality for early fusion: ' + net_type))
-        with open(os.path.join(path, split + '.prototxt'), 'w') as f:
-            f.write(str(net_archs.fcn_early(split, tops).to_proto()))
+        net = net_archs.fcn_early(split, tops)
+    elif '_conv' in net_type:
+        if 'd' in net_type or 'D' in net_type:
+            tops = ['color', 'depth', 'label']
+        elif 'hha2' in net_type or 'HHA2' in net_type:
+            tops = ['color', 'hha2', 'label']
+        else:
+            raise(Exception('Unkown modality for conv fusion: ' + net_type))
+        net = net_archs.fcn_conv(split, tops)
     else:
         raise(Exception('net_type "' + net_type +
                         '" unrecognised, create case for new network here.'))
+    with open(os.path.join(path, split + '.prototxt'), 'w') as f:
+        f.write(str(net.to_proto()))
 
 
-def print_all_nets():
+def print_rgb_nets():
     tops = ['color', 'label']
     with open('trainval.prototxt', 'w') as f:
         f.write(str(net_archs.fcn('train', tops).to_proto()))
@@ -81,5 +97,13 @@ def print_all_nets():
     with open('test.prototxt', 'w') as f:
         f.write(str(net_archs.fcn('test', tops).to_proto()))
 
+
+def test_print(net_type='rgb'):
+    n = createNet('trainval', net_type=net_type)
+    print_net('', split='trainval', net_type=net_type)
+    print_net('', split='val', net_type=net_type)
+    print_net('', split='test', net_type=net_type)
+
 if __name__ == '__main__':
-    print_all_nets()
+    n_type = 'rgbd_conv'
+    test_print(n_type)
