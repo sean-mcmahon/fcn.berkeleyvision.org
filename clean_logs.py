2376da0e4423d6a07997ff67edb0bc08a3a0f288
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-04-27 09:13:56 +1000

    code is much cleaner, need to work out the log writing scheme for final debugging

--- clean_logs.py
+++ clean_logs.py
@@ -32,6 +32,7 @@ def log_search(walk_iterator, pattern):
     print '-' * 20
     return logfile_names
 
+
 def find_next_py_line(logfile, beg, debug=False):
     # Find next line without I0
     # End of the c++ print (first after line after match not starting
@@ -49,21 +50,32 @@ def find_next_py_line(logfile, beg, debug=False):
     return next_py_line
 
 
-if __name__ == '__main__':
-    # walk through directory and find .log files.
-    # dir_ = '/home/sean/hpc-home/Fully-Conv-Network/Resources/' + \
-    #     'FCN_paramsearch/rgb_workers/rgb_1_23/'
-    # dir_ = '/home/sean/Dropbox/Uni/Code/FCN_models'
-    dir_ = '/home/sean/Documents/logfix_test/'
+def replace_txt(logfile_in, beg_id, end_id, pattern, new_txt="\n"):
+    # add a margin arounf the ids
+    begin = beg_id - 5
+    end = end_id + 6
+    removal_area = logfile_in[begin: end]
+    bef_rem = logfile_in[:begin]
+    after_rem = logfile_in[end:]
+    removed_area = removal_area.replace(pattern, new_txt, 1)
 
+    return bef_rem + removed_area + after_rem
+
+
+def add_txt(logfile_in, insertion_p, new_txt):
+    log_bef = logfile_in[:insertion_p]
+    log_after = logfile_in[insertion_p + 1:]
+    return log_bef + new_txt + log_after
+
+
+def main(log_dir):
     print 'walkin...'
     walk_start = time.time()
-    folders = os.walk(dir_)
+    folders = os.walk(log_dir)
     print 'Walk took {} seconds'.format(time.time() - walk_start)
 
     logfile_names = log_search(folders, '*.log')
 
-    start_t = time.time()
     for logfile_name in logfile_names:
         # regex matching!
         with open(logfile_name, 'r') as f:
@@ -81,31 +93,33 @@ if __name__ == '__main__':
             # print '---> First {} before "{}" match'.format(r'\n', r'[^\n]I0')
             # print repr(logfile[first_nl: match.end(0) + 5])
             line_beg = logfile[first_nl: match.start(0) + 1]
-            # print '{}'.format(repr(line_beg))
 
             newlines = find_next_py_line(logfile, match.end(0))
 
             # add line_beg text to next non I0 line
-            log_bef = logfile[:newlines]
-            log_after = logfile[newlines + 1:]
-            new_log = log_bef + line_beg + log_after
+            new_log = add_txt(logfile, newlines, line_beg)
             # remove text at line_beg. Make sure there are no other matches
-            l_id = first_nl - 5
-            h_id = match.start(0) + 6
-            removal_area = new_log[l_id: h_id]
-            bef_rem = new_log[:l_id]
-            after_rem = new_log[h_id:]
-            removed_area = removal_area.replace(line_beg, "\n", 1)
-            logfile = bef_rem + removed_area + after_rem
+            logfile = replace_txt(new_log, first_nl, match.start(0), line_beg)
 
-            # logfile = new_log.replace(line_beg, "\n", 1)
-            # line begin should start with a "\n"
-            # print '+' * 30
-            # print new_log2
         print '{} matches found in {}'.format(count, os.path.basename(logfile_name))
         log_dr, base_name = os.path.split(logfile_name)
         with open(logfile_name + 'FIXED', 'w') as f:
             f.write(logfile)
         print 'saved ', logfile_name + 'FIXED'
+
+
+@click.command()
+@click.argument('log_dir', nargs=1, type=click.Path(exists=True))
+def main_click(log_dir):
+    # walk through directory and find .log files.
+    # dir_ = '/home/sean/hpc-home/Fully-Conv-Network/Resources/' + \
+    #     'FCN_paramsearch/rgb_workers/rgb_1_23/'
+    # dir_ = '/home/sean/Dropbox/Uni/Code/FCN_models'
+    # dir_ = '/home/sean/Documents/logfix_test/'
+    start_t = time.time()
+    main(log_dir)
     duration = time.time() - start_t
     print ' Log fixing took {} seconds'.format(duration)
+
+if __name__ == '__main__':
+    main_click()
