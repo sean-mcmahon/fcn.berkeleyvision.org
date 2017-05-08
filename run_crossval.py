82afa47de1d2b7386a61c8877c4f0dee4d2f9697
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-05-08 15:44:02 +1000

    made copies of the solver and worker scripts to run most the modalities at once

--- run_crossval.py
+++ run_crossval.py
@@ -15,20 +15,26 @@ if __name__ == '__main__':
     fold_idx = range(n_folds)
     fold_idx_str = [str(s + 1) + '_4' for s in fold_idx]
     # fold_idx_str = ['4_4']
-    n_type = 'rgbhha_early_nyurgb'
-    parent_dir = n_type + '_crossval'
-    cross_val_dir = os.path.join(file_location, parent_dir)
-    print cross_val_dir
-    if not os.path.isdir(cross_val_dir):
-        os.mkdir(cross_val_dir)
-    base_lr = 1e-10
-    net_type = 'rgbhha2_early'
-    net_init = 'NYU_rgb'
-    print '-'*20
-    for idx in fold_idx_str:
-        worker_dir = os.path.join(cross_val_dir, n_type + '_'+idx)
-        print worker_dir
-        job_id = run_worker(worker_dir, 'worker', fold_idx=idx)
-        print 'job id = ', job_id
-        time.sleep(15)  # my attempt to fix no cuda capable device error
-        # (when there are cuda capable devices)
+    # n_type = 'hha'
+    net_types = ['lateMixrgbd', 'depth', 'earlyrgbd',
+                 'earlyrgbhha', 'lateMixrgbhha']
+    for n_type in net_types:
+        parent_dir = n_type + '_crossval2'
+        cross_val_dir = os.path.join(file_location, parent_dir)
+        print '=' * 50
+        print cross_val_dir
+        if not os.path.isdir(cross_val_dir):
+            os.mkdir(cross_val_dir)
+        # base_lr = 1e-10
+        # net_type = 'rgbhha2_early'
+        # net_init = 'NYU_rgb'
+        print '-' * 20
+        for idx in fold_idx_str:
+            worker_dir = os.path.join(cross_val_dir, n_type + '_' + idx)
+            print worker_dir
+            job_id = run_worker(
+                worker_dir, 'worker_{}_cv'.format(n_type), fold_idx=idx)
+            print 'running worker: {}'.format('worker_{}_cv'.format(n_type))
+            print 'job id = ', job_id
+            time.sleep(15)  # my attempt to fix no cuda capable device error
+            # (when there are cuda capable devices)
