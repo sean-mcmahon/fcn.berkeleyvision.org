7b352846ae49c8ffcada9448006c956692cc0b87
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-05-14 13:12:42 +1000

    fixed up mv log files

--- mv_job_scripts.bash
+++ mv_job_scripts.bash
@@ -1,8 +1,10 @@
 #!/bin/bash -l
 
-mv paramSearch.e* hpc-logs/
-mv paramSearch.o* hpc-logs/
-mv worker.o* hpc-logs/
-mv workder.e* hpc-logs/
-mv master.e* hpc-logs/
-mv master.o* hpc-logs/
+# mv paramSearch.e* hpc-logs/
+# mv paramSearch.o* hpc-logs/
+# mv worker.o* hpc-logs/
+# mv workder.e* hpc-logs/
+# mv master.e* hpc-logs/
+# mv master.o* hpc-logs/
+mv -v *.o* hpc-logs/
+mv -v *.e* hpc-logs/
